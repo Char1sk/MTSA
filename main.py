7578210fe4aae18ce728b4d249e70af95c6beaaf
Char1sk <2553036255@qq.com>
2023-10-24 20:44:30 +0800

    knn_test, job select, typo

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from src.models.TsfKNN import TsfKNN
-from src.models.baselines import ZeroForecast, MeanForecast, LinearRegression, ExponantielSmoothing
+from src.models.baselines import ZeroForecast, MeanForecast, LinearRegression, ExponantialSmoothing
 from src.utils.transforms import IdentityTransform, NormalizationTransform, StandardizationTransform, MeanNormalizationTransform, BoxCoxTransform
 from trainer import MLTrainer
 from src.dataset.dataset import get_dataset
@@ -14,7 +14,7 @@ def get_model(args):
         'ZeroForecast': ZeroForecast,
         'MeanForecast': MeanForecast,
         'LinearRegression': LinearRegression,
-        'ExponantielSmoothing': ExponantielSmoothing,
+        'ExponantielSmoothing': ExponantialSmoothing,
         'TsfKNN': TsfKNN,
     }
     return model_dict[args.model](args)
@@ -65,7 +65,10 @@ def get_args():
 
     # transform define
     parser.add_argument('--transform', type=str, default='IdentityTransform')
-    parser.add_argument('--boxcox_lambda', type=float, default=1.0, help='hyper-parameter lambda in BoxCox')
+    parser.add_argument('--boxcox_lambda', type=float, default=2.0, help='hyper-parameter lambda in BoxCox')
+
+    # job
+    parser.add_argument('--job', type=str, default='main', help='select in ["main", "test", "knn_test"]')
 
     args = parser.parse_args()
     return args
@@ -78,12 +81,7 @@ def set_args(args, dataset, datapath, transform, model):
     args.model = model
 
 
-def main():
-    fix_seed = 2023
-    random.seed(fix_seed)
-    np.random.seed(fix_seed)
-
-    args = get_args()
+def main(args):
     # load dataset
     dataset = get_dataset(args)
     # create model
@@ -98,17 +96,15 @@ def main():
     trainer.train()
     # evaluate model
     trainer.evaluate(dataset, seq_len=args.seq_len, pred_len=args.pred_len)
+    
     time2 = time.time()
     print(f"time: {time2-time1}")
-    
-    
 
 
-def test():
+def test(args):
     fix_seed = 2023
     random.seed(fix_seed)
     np.random.seed(fix_seed)
-    args = get_args()
     
     datasets = ['M4', 'ETT', 'Custom']
     datapaths = {
@@ -118,7 +114,7 @@ def test():
         'traffic': './dataset/traffic/traffic.csv',
         'weather': './dataset/weather/weather.csv'
     }
-    models = ['ZeroForecast','MeanForecast','LinearRegression','ExponantielSmoothing']
+    models = ['ZeroForecast','MeanForecast','LinearRegression','ExponantielSmoothing', 'TsfKNN']
     transforms = [
         'IdentityTransform',
         'NormalizationTransform',
@@ -134,6 +130,8 @@ def test():
             for dp in datapaths.keys():
                 for md in models:
                     for tf in transforms:
+                        random.seed(fix_seed)
+                        np.random.seed(fix_seed)
                         print(dp,md,tf)
                         set_args(args, ds, datapaths[dp], tf, md)
                         dataset = get_dataset(args)
@@ -144,9 +142,54 @@ def test():
                         results = trainer.evaluate(dataset, seq_len=args.seq_len, pred_len=args.pred_len)
                         line = [dp, md, tf] + [str(r) for r in results]
                         f.write(','.join(line)+'\n')
-                    
+
+
+def knn_test(args):
+    fix_seed = 2023
+    
+    np.set_printoptions(linewidth=200)
+    for (dataset, datapath) in [('Custom', './dataset/illness/national_illness.csv'), ('ETT', './dataset/ETT-small/ETTh1.csv')]:
+        # set_args(args, dataset, datapath, 'BoxCoxTransform', 'TsfKNN')
+        set_args(args, dataset, datapath, 'IdentityTransform', 'TsfKNN')
+        for approx in [None, 'LSH']:
+            args.approx = approx
+            dataset = get_dataset(args)
+            model = get_model(args)
+            transform = get_transform(args)
+            # Get time and results
+            results_list = []
+            times_list = []
+            rounds = 5
+            for i in range(rounds+1):
+                random.seed(fix_seed)
+                np.random.seed(fix_seed)
+                # Dont count in 1st time without cache
+                trainer = MLTrainer(model=model, transform=transform, dataset=dataset)
+                if i != 0:
+                    time1 = time.time()
+                trainer.train()
+                results = trainer.evaluate(dataset, seq_len=args.seq_len, pred_len=args.pred_len)
+                if i != 0:
+                    time2 = time.time()
+                if i != 0:
+                    times_list.append(time2-time1)
+                    results_list.append(results)
+            # Calculate time and results
+            results_mean = np.array(results_list).mean(axis=0)
+            times_mean = np.array(times_list).mean(axis=0)
+            # Print results
+            print(datapath, approx)
+            print(f'\t{results_mean}, {times_mean}s')
 
 
 if __name__ == '__main__':
-    main()
-    # test()
+    jobs = {
+        'main': main,
+        'test': test,
+        'knn_test': knn_test,
+    }
+    args = get_args()
+    jobs[args.job](args)
+    # main(args)
+    # test(args)
+    # knn_test(args)
